486e3fabc0b70d011ad2e8585337d491c21f9718
varitovaro1 <133014817+varitovaro1@users.noreply.github.com>
2025-04-08 23:49:14 -0400

    terminado

---
+++ estadoBateria/Service1.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace estadoBateria
+{
+    partial class estadoBateria
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // estadoBateria
+            // 
+            this.ServiceName = "estadoBateria";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ estadoBateria/Service1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace estadoBateria
+{
+    public partial class estadoBateria: ServiceBase
+    {
+        public estadoBateria()
+        {
+            InitializeComponent();
+
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("estadoBateria")) {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "estadoBateria", "Application");
+            }
+            eventLog1.Source = "estadoBateria";
+            eventLog1.Log = "Application";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Inicio Monitoreo bateria");
+        }
+ 
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Pausa Monitoreo bateria");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string Porcentaje;
+
+            Porcentaje = (SystemInformation.PowerStatus.BatteryLifePercent * 100).ToString() + "%";
+            eventLog1.WriteEntry("Porsentaje actual de la bateria: " + Porcentaje);
+        }
+    }
+}
